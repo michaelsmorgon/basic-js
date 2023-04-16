a2fe0d6435d1de8f88910372ae2728c5e4ba3e12
michaelsmorgon <smorgon@int.pl>
2023-04-16 20:38:39 +0200

    feat: add vigener cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,85 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  constructor (directType = true) {
+    this.directType = !!directType;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+
+    key = (key.match(/[a-zA-Z]+/gui)).join('');
+    
+    let encryptMessage = [];
+    let shift = 0;
+    let shiftKey = 0;
+    message.split('').forEach((val, ind) => {
+      let indexMessageSymbol = this.string.indexOf(val.toUpperCase());
+      if (indexMessageSymbol === -1) {
+        encryptMessage.push(val.toUpperCase());
+        shift++;
+        return;
+      }
+      let indexKeySymbol = this.string.indexOf(key.toUpperCase()[(ind - shift) % key.length]);
+      while(indexKeySymbol === -1) {
+        shiftKey++;
+        indexKeySymbol = this.string.indexOf(key.toUpperCase()[(ind - shift) % key.length + shiftKey]);
+      }
+      let encryptIndex = indexKeySymbol + indexMessageSymbol;
+
+      if ( encryptIndex > 25) {
+        encryptIndex -= 26;
+      }
+      encryptMessage.push(this.string[encryptIndex]);
+    });
+
+    if (!this.directType) {
+      encryptMessage = encryptMessage.reverse();
+    }
+
+    return encryptMessage.join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+
+    key = (key.match(/[a-zA-Z]+/gui)).join('');
+    
+    let decryptedMessage = [];
+    let shift = 0;
+
+    encryptedMessage.split('').forEach((val, ind) => {
+      let indexMessageSymbol = this.string.indexOf(val.toUpperCase());
+      if (indexMessageSymbol === -1) {
+        decryptedMessage.push(val.toUpperCase());
+        shift++;
+        return;
+      }
+
+      let indexKeySymbol = this.string.indexOf(key.toUpperCase()[(ind - shift) % key.length]);
+      while(indexKeySymbol === -1) {
+        shiftKey++;
+        indexKeySymbol = this.string.indexOf(key.toUpperCase()[(ind - shift) % key.length + shiftKey]);
+      }
+      let decryptIndex = indexMessageSymbol - indexKeySymbol;
+
+      if ( decryptIndex < 0) {
+        decryptIndex += 26;
+      }
+      decryptedMessage.push(this.string[decryptIndex]);
+    });
+
+    if (!this.directType) {
+      decryptedMessage = decryptedMessage.reverse();
+    }
+
+    return decryptedMessage.join('');
   }
 }
 
